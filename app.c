c5e3a783a468e6bbfb1c09fa6e1e5f8a949c3b06
Rafael Escalante <copperx@users.noreply.github.com>
2025-11-13 02:19:33 +0000

    Created app

---
+++ app.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello!\n");
+    printf("Enter an integer: ");
+    int number;
+    scanf("%d", &number);
+    printf("You entered %d\n", number);
+    return 0;
+}
